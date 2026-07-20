81cd09df32573449e6cbfd10d9940b54ae182827
imam-hossain-wd <zunayedemon6t9@gmail.com>
2026-07-20 23:11:13 +0600

    added app server and env configaration file and project setup complete

---
+++ src/app.ts
@@ -0,0 +1,38 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express, { Request, Response } from "express";
+import expressSession from "express-session";
+import passport from "passport";
+import { envVars } from "./app/config/env";
+
+
+
+const app = express()
+
+
+app.use(expressSession({
+    secret: envVars.EXPRESS_SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(cookieParser())
+app.use(express.json())
+app.set("trust proxy", 1);
+app.use(express.urlencoded({ extended: true }))
+app.use(cors({
+    origin: envVars.FRONTEND_URL,
+    credentials: true
+}))
+
+// app.use("/api/v1", router)
+
+app.get("/", (req: Request, res: Response) => {
+    res.status(200).json({
+        message: "🌍 Travel, Hotel, Tour & Vehicle Management System"
+    })
+})
+
+
+export default app
