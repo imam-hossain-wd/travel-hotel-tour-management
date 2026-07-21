69b970396c2c679825afc3fad5318f4599a689e1
imam-hossain-wd <zunayedemon6t9@gmail.com>
2026-07-21 15:08:52 +0600

    successfully implement credientials login module

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -9,15 +9,16 @@ import { catchAsync } from "../../utils/catchAsync";
 import { sendResponse } from "../../utils/sendResponse";
 
 
+
 const createUser = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const user = await UserServices.createUser(req.body)
-
     sendResponse(res, {
         success: true,
         statusCode: httpStatus.CREATED,
         message: "User Created Successfully",
         data: user,
     })
+
 })
 const updateUser = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const userId = req.params.id;
