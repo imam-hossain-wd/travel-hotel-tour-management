b31f891c21435b256d6ccf31a020a2e8671107e6
imam-hossain-wd <zunayedemon6t9@gmail.com>
2026-07-21 18:39:35 +0600

    added division controller and services

---
+++ src/app/modules/division/division.controller.ts
@@ -0,0 +1,77 @@
+import { Request, Response } from "express";
+
+import { catchAsync } from "../../utils/catchAsync";
+import { sendResponse } from "../../utils/sendResponse";
+import { IDivision } from "./division.interface";
+import { DivisionService } from "./division.service";
+
+const createDivision = catchAsync(async (req: Request, res: Response) => {
+    const parsedData = JSON.parse(req.body.data);
+
+    const payload: IDivision = {
+        ...parsedData,
+        thumbnail: req.file?.path
+    };
+    const result = await DivisionService.createDivision(payload);
+    sendResponse(res, {
+        statusCode: 201,
+        success: true,
+        message: "Division created",
+        data: result,
+    });
+});
+
+const getAllDivisions = catchAsync(async (req: Request, res: Response) => {
+    const query = req.query;
+    const result = await DivisionService.getAllDivisions(query as Record<string, string>);
+    sendResponse(res, {
+        statusCode: 200,
+        success: true,
+        message: "Divisions retrieved",
+        data: result.data,
+        meta: result.meta,
+    });
+});
+const getSingleDivision = catchAsync(async (req: Request, res: Response) => {
+    const slug = req.params.slug
+    const result = await DivisionService.getSingleDivision(slug as string);
+    sendResponse(res, {
+        statusCode: 200,
+        success: true,
+        message: "Divisions retrieved",
+        data: result.data,
+    });
+});
+
+const updateDivision = catchAsync(async (req: Request, res: Response) => {
+    const id = req.params.id;
+    const payload: IDivision = {
+        ...req.body,
+        thumbnail: req.file?.path
+    }
+    const result = await DivisionService.updateDivision(id as string, payload);
+    sendResponse(res, {
+        statusCode: 200,
+        success: true,
+        message: "Division updated",
+        data: result,
+    });
+});
+
+const deleteDivision = catchAsync(async (req: Request, res: Response) => {
+    const result = await DivisionService.deleteDivision(req.params.id as string);
+    sendResponse(res, {
+        statusCode: 200,
+        success: true,
+        message: "Division deleted",
+        data: result,
+    });
+});
+
+export const DivisionController = {
+    createDivision,
+    getAllDivisions,
+    getSingleDivision,
+    updateDivision,
+    deleteDivision,
+};
